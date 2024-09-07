102dabaa1644b8a14be599fc9e382ba7ac9a4f05
Patrick <plmiranda@rdretailgroup.com.ph>
2024-09-07 10:59:39 +0800

    minor

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -45,11 +45,11 @@ const DashboardPage: React.FC<DashboardPageProps> = async ({
           </Card>
           <Card>
             <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-              <CardTitle className="text-sm font-medium">Sales</CardTitle>
+              <CardTitle className="text-sm font-medium">Fullfilled Orders</CardTitle>
               <CreditCard className="h-4 w-4 text-muted-foreground" />
             </CardHeader>
             <CardContent>
-              <div className="text-2xl font-bold">+{salesCount}</div>
+              <div className="text-2xl font-bold">{salesCount}</div>
             </CardContent>
           </Card>
           <Card>
